b9b43451929320130318a181a7dc285080acf7bd
Samantha <140463087+Samfreeman98@users.noreply.github.com>
2024-02-12 19:23:33 -0700

    Update root.render

--- src/index.jsx
+++ src/index.jsx
@@ -10,6 +10,5 @@ const App = () => {
 // Finds the root of your app
 const container = document.querySelector("#root");
 const root = createRoot(container);
-
 // Tells React to render your app in the root DOM element
-root.render(<MyFlixApplication />);
+root.render(<App />);
