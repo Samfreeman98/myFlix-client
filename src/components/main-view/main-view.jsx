46554af6c8b7ed6c13cab14242ac5fdc39a21449
Samantha <140463087+Samfreeman98@users.noreply.github.com>
2024-02-13 19:54:58 -0700

    Add array of movies

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,11 +1,12 @@
 export const MainView = () => {
+    const [movies, setMovies] = useState([
+        { id: 1, title: "Inception", image: "https://m.media-amazon.com/images/S/pv-target-images/2b8bc4866e28f9b95c65965a2b33c8b14a8cebe8287b7399b536c672729ef018.jpg", director: "Christopher Nolan" },
+        { id: 2, title: "Get Out", image: "https://m.media-amazon.com/images/I/91yhYRckRvL._AC_UF894,1000_QL80_.jpg", director: "Jordan Peele" },
+        { id: 3, title: "The Godfather", image: "https://m.media-amazon.com/images/I/71mfJsyJO4L._AC_UF894,1000_QL80_.jpg", director: "Francis Ford Coppola" },
+        { id: 4, title: "Jurrasic Park", image: "https://m.media-amazon.com/images/I/81AGqBcpYOL._AC_UF894,1000_QL80_.jpg", director: "Steven Spielberg" }
+    ]);
     return (
         <div>
-            <div>Eloquent JavaScript</div>
-            <div>Mastering JavaScript Functional Programming</div>
-            <div>JavaScript: The Good Parts</div>
-            <div>JavaScript: The Definitive Guide</div>
-            <div>The Road to React</div>
         </div>
     );
 };
