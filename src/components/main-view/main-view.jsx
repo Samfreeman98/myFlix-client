af3b63b532c883a16cadc28c1aa7d876225ecc6f
Samantha <140463087+Samfreeman98@users.noreply.github.com>
2024-02-13 19:57:04 -0700

    Add import statments for MovieCard and MovieView

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,3 +1,6 @@
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
+
 export const MainView = () => {
     const [movies, setMovies] = useState([
         { id: 1, title: "Inception", image: "https://m.media-amazon.com/images/S/pv-target-images/2b8bc4866e28f9b95c65965a2b33c8b14a8cebe8287b7399b536c672729ef018.jpg", director: "Christopher Nolan" },
@@ -5,8 +8,15 @@ export const MainView = () => {
         { id: 3, title: "The Godfather", image: "https://m.media-amazon.com/images/I/71mfJsyJO4L._AC_UF894,1000_QL80_.jpg", director: "Francis Ford Coppola" },
         { id: 4, title: "Jurrasic Park", image: "https://m.media-amazon.com/images/I/81AGqBcpYOL._AC_UF894,1000_QL80_.jpg", director: "Steven Spielberg" }
     ]);
+    }
+
+    if (movies.length === 0) {
+        return <div>The list is empty!</div>
+    }
+
     return (
         <div>
+            {movies.map((movie) => (
         </div>
     );
 };
