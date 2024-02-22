84f19f21e69fa41bcd7af568aab4351152ec6623
Samantha <140463087+Samfreeman98@users.noreply.github.com>
2024-02-21 20:03:24 -0700

    Feat: add useEffect hook

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
@@ -6,6 +6,22 @@ export const MainView = () => {
     const [movies, setMovies] = useState([]);
 
     const [selectedMovie, setSelectedMovie] = useState(null);
+
+    useEffect(() => {
+        fetch(CONNECTION_URI)
+            .then((response) => response.json())
+            .then((data) => {
+                const moviesFromApi = data.docs.map((doc) => {
+                    return {
+                        id: doc.key,
+                        title: doc.title,
+                        image: `https://covers.openlibrary.org/b/id/${doc.cover_i}-L.jpg`,
+                        director: doc.director_name?.[0]
+                    };
+                });
+                setMovies(moviesFromApi)
+            });
+    }, []);
 
     if (selectedMovie) {
         return (<MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
